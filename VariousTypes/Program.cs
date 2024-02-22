46fc4cf9603044801b1a4fb762bfbdb2107ec49e
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-22 16:27:36 +0000

    Add Integral Variable Types

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Variáveis inteiras com literais adequados
+            sbyte num_sb = -69;
+            Console.WriteLine($"Valor do tipo sbyte -> {num_sb}");
+
+            short num_shr = 27534;
+            Console.WriteLine($"Valor do tipo short -> {num_shr}");
+
+            int num_int = -215;
+            Console.WriteLine($"Valor do tipo int -> {num_int}");
+
+            long num_lng = 2611512584356L;
+            Console.WriteLine($"Valor do tipo long -> {num_lng}");
+
+            byte num_bt = 7;
+            Console.WriteLine($"Valor do tipo byte -> {num_bt}");
+
+            ushort num_ush = 42561;
+            Console.WriteLine($"Valor do tipo ushort -> {num_ush}");
+
+            uint num_uint = 211562280U;
+            Console.WriteLine($"Valor do tipo uint -> {num_uint}");
+
+            ulong num_ulg = 243562934UL;
+            Console.WriteLine($"Valor do tipo ulong -> {num_ulg}");
+
+            char vlr_char = 'f';
+            Console.WriteLine($"Valor do tipo char -> {vlr_char}");
+
+
+
+
+        }
+           
+            
+
+    }
+}
