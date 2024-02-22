6c43a40c528c14791cadffde0f07797094f2e8e5
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-22 20:52:59 +0000

    String conversion to Integral

---
+++ AskForInput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input de strings pelo utilizador e conversão para valores
+
+            Console.WriteLine("Por favor insira um número inteiro.");
+            string str1 = Console.ReadLine();
+            int inteiro = int.Parse(str1);
+
+
+
+        
+
+        }
+    }
+}
