e1a2df4ba0b6066e12a22b03279834fad0cd4f33
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-22 18:02:57 +0000

    Add Strings Characters

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {   // String com caracteres de escape
+
+            string str = "Olá, isto é uma plica -> \',\n\tIsto foi um enter e um tab,\ne já agora observe uma barra invertida -> \\";
+            Console.WriteLine(str);
+
+            // String com caracteres de Unicode
+
+            string UC = "Um telefone \u260f e uma carta \u2709";        
+            Console.WriteLine(UC);
+
+            // String com caracteres de escape e de Unicode
+
+            string frase = "Uma aspa \" e um símbolo de gelo \u2746";
+            Console.WriteLine(frase);
+
+        }
+    }
+}
