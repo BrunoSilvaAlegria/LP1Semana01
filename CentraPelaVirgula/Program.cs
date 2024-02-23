c5e9b7e43ac2707967c81248acede9b29eab9230
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-23 13:31:13 +0000

    Input's

---
+++ CentraPelaVirgula/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CentraPelaVirgula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Por favor insira um número real.");
+            string str1 = Console.ReadLine();
+
+            Console.WriteLine("Por favor insira um número real.");
+            string str2 = Console.ReadLine();
+
+            Console.WriteLine("Por favor insira um número real.");
+            string str3 = Console.ReadLine();
+
+            Console.WriteLine("Por favor insira um número real.");
+            string str4 = Console.ReadLine();
+        }
+    }
+}
