e5f645ecdf1fd26ad7ae8a74a5817d8cf48a6cf1
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-23 14:00:57 +0000

    Almost fixed

--- CentraPelaVirgula/Program.cs
+++ CentraPelaVirgula/Program.cs
@@ -6,17 +6,25 @@ namespace CentraPelaVirgula
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Por favor insira quatro números reais.");
+            // Pedido dos 4 números reais
+            Console.WriteLine("Por favor insira quatro números reais."); 
             string str1 = Console.ReadLine();
             string str2 = Console.ReadLine();
             string str3 = Console.ReadLine();
             string str4 = Console.ReadLine();
 
-            Console.WriteLine($"{str1,10:f2}");   
-            Console.WriteLine($"{str2,10:f2}"); 
-            Console.WriteLine($"{str3,10:f2}"); 
-            Console.WriteLine($"{str4,10:f2}");           
+            //Conversão das strings para número real(float)
+            float num1 = float.Parse(str1);
+            float num2 = float.Parse(str2);
+            float num3 = float.Parse(str3);
+            float num4 = float.Parse(str4);
+
+            // Impressão dos valores com alinhamento à direita
+            // e com duas casas decimais
+            Console.WriteLine($"{num1,10:f2}");   
+            Console.WriteLine($"{num2,10:f2}"); 
+            Console.WriteLine($"{num3,10:f2}"); 
+            Console.WriteLine($"{num4,10:f2}");           
         }
     }
 }
